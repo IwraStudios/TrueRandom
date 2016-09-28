664dbad6a43274dab20c5bc002a6d8c0ad1e4fb3
unknown <Administrator@W233.jbc.local>
2016-09-28 12:05:31 +0200

    Changed and added some functions

--- Gravity.Random/GravityRandom.cs
+++ Gravity.Random/GravityRandom.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Gravity.TrueRandom
 {
@@ -10,7 +10,7 @@ namespace Gravity.TrueRandom
     {
 
     }
-    public class RandomUtils 
+    public class RandomUtils
     {
         ///The buttom layer of the library
         ///Meant to be used in advanced programs
@@ -35,25 +35,41 @@ namespace Gravity.TrueRandom
             return BitConverter.ToInt32(bytes, 0);
         }
 
-        public string ByteArrayToString(byte[] ba)
+        public string ByteArrayToHex(byte[] ba)
         {
             StringBuilder hex = new StringBuilder(ba.Length * 2);
             foreach (byte b in ba) hex.AppendFormat("{0:x2}", b);
             return hex.ToString();
         }
+        public string ByteArrayToString(byte[] ba){ //UTF8 Only for now TODO: added other encodings
+          return System.Text.Encoding.UTF8.GetString(ba, 0, ba.Length);
+        }
+        public int InefficientRandomInt(int min = int.MinValue, int max = int.MaxValue){
+          Thread.Sleep(this.GetPseudoRandomNumber(0, 1, 50));
+          int time = Int32.Parse(System.DateTime.Now.ToString("fff")); //Same
+          int ran = this.GetPseudoRandomNumber(time, min, max);
+        }
+        public int EfficientRandomInt(int min = int.MinValue, int max = int.MaxValue){
+          //Thread.Sleep(this.GetPseudoRandomNumber(0, 1, 50));
+          int time = Int32.Parse(System.DateTime.Now.ToString("fff")); //Same
+          int ran = this.GetPseudoRandomNumber(time, min, max);
+        }
     }
 
-    public class RandomGenerator 
+    public class RandomGenerator
     {
         ///Top layer of the library
         ///Meant to be used in general purpose programs
         public byte[] GetRandomBytes(uint AmountOfBytes = 4, int min = int.MinValue, int max = int.MaxValue)
-        { //minimum amount of bytes is 4
+        {
+            if (AmountOfBytes < 4) AmountOfBytes = 4;
             byte[][] tmp = new byte[AmountOfBytes/4][];
             for (uint i = 0; i > AmountOfBytes/4; i++)
             {
-                int d = new RandomUtils().GetPseudoRandomNumber(int.Parse(DateTime.Now.ToString("fff")), min, max); //TODO: Add more randomness
-                tmp[i] = BitConverter.GetBytes(d);
+                int time = Int32.Parse(System.DateTime.Now.ToString("fff")); //Same
+                int d = new RandomUtils().GetPseudoRandomNumber(time, min, max); //TODO: Add more randomness
+                //byte[] b = System.BitConverter.GetBytes(d);
+                tmp[i] = System.BitConverter.GetBytes(d);
             }
             if((AmountOfBytes % 4) != 0)Array.Resize<byte>(ref tmp[tmp.Length - 1], Convert.ToInt32(AmountOfBytes % 4)); //Fixes only 4 * n issue
             return new RandomUtils().CombineByteArray(tmp);
